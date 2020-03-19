3c4d3275929c603e783766f7cdc438dd110f26ad
Daniel Kent <Kentpercussion@gmail.com>
2020-03-19 00:27:54 -0600

    001

--- 001.js
+++ 001.js
@@ -1,2 +1,13 @@
 // Implement an algorithm to determine if a string has all unique characters
 
+const hasOnlyUniqueChars = (str) => {
+  const map = {};
+
+  for (let i=0; i<str.length; i++) {
+    const char = str[i];
+    if (map[char]) return false;
+    map[char] = true;
+  }
+
+  return true;
+};
